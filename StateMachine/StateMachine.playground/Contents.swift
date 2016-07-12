298f29e7f46937dd1fb47f3bec0a18b522e6d141
Pushan Mitra <pushanmitra@Pushans-MacBook-Pro.local>
2016-07-12 16:53:54 +0530

    Initial Commit

---
+++ StateMachine/StateMachine.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Events : String, EventDescriptor {
+    case One,Two
+}
+
+enum States : String, StateIdentifier {
+    case X,Y,Z
+}
+
+class StateX : State<States,[String: Any],Events> {
+    override func operation(_ event: Events, _ store: [String : Any]?, _ completion: Completion) {
+        print("State X ----- Proccessing Event : \(event)")
+        completion(next: States.Y, store: nil)
+    }
+}
+
+class StateY : State<States,[String: Any],Events> {
+    override func operation(_ event: Events, _ store: [String : Any]?, _ completion: Completion) {
+        print("State Y ----- Proccessing Event : \(event)")
+        completion(next: States.X, store: nil)
+    }
+}
+
+let stateMachine : StateMachine<States,[String : Any], Events> = StateMachine<States,[String : Any], Events>()
+
+let x: StateX = StateX(state: .X)
+let y: StateY = StateY(state: .Y)
+stateMachine.addState(x)
+stateMachine.addState(y)
+
+try! stateMachine.start(state: .X)
+
+stateMachine.handleEvent(.One, nil)
+print("gap")
+stateMachine.handleEvent(.Two, nil)
