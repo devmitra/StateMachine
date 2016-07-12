ffe34786ae78531d9be7c7b974e726d858631473
Pushan Mitra <pushanmitra@Pushans-MacBook-Pro.local>
2016-07-12 17:57:09 +0530

    Add lib struture of project

---
+++ StateMachineLib/StateMachineLib.h
@@ -0,0 +1,19 @@
+//
+//  StateMachineLib.h
+//  StateMachineLib
+//
+//  Created by Pushan Mitra on 12/07/16.
+//  Copyright © 2016 Pushan Mitra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateMachineLib.
+FOUNDATION_EXPORT double StateMachineLibVersionNumber;
+
+//! Project version string for StateMachineLib.
+FOUNDATION_EXPORT const unsigned char StateMachineLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachineLib/PublicHeader.h>
+
+
